a6e5a23ffd33cf6ea3968d2db481c5e805b6a0ac
Adrien Dion <adion81@gmail.com>
2020-05-26 14:11:46 -0500

    Ill Tempered Penguins

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function handleSubmit(e){
+    e.preventDefault();
+    var table = document.getElementById("table-body");
+    var row = document.createElement('tr');
+    var name = document.createElement('td');
+    var email = document.createElement('td');
+    var inputName = document.getElementById("name");
+    var inputEmail = document.getElementById("email");
+    name.innerHTML = inputName.value;
+    email.innerHTML = inputEmail.value;
+    row.appendChild(name);
+    row.appendChild(email);
+    table.appendChild(row);
+    inputName.value = "";
+    inputEmail.value = "";
+}
